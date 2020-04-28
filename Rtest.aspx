0c5ea743c7c682bcb7f031f1c90f6f861f75a172
elaborative <ryan@ryancooper.com>
2020-04-28 16:05:31 -0700

    Add files via upload

---
+++ Rtest.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="VB" AutoEventWireup="false" CodeFile="Rtest.aspx.vb" Inherits="admin_Rtest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+       Name:<asp:Textbox ID="TxtName" runat="server" Text="Name" />
+       Max:<asp:Textbox ID="TxtValue" runat="server" Text="100" />
+       <asp:Button ID="BtnRefresh" runat="server" Text="Refresh" />
+       <asp:Label ID="LblPlots" runat="server"></asp:Label>
+    <div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Rtest.aspx.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Diagnostics
+
+Partial Class admin_Rtest
+    Inherits System.Web.UI.Page
+
+    Private Sub BtnRefresh_Click(sender As Object, e As EventArgs) Handles BtnRefresh.Click
+        Dim strName As String = ""
+        Dim strMax As String = ""
+        strName = TxtName.Text
+        strMax = TxtValue.Text
+
+        Dim strReturned = RunRScript("C:\Program Files\R\R-4.0.0\bin\Rscript.exe", "D:\RScripts\randohist.R \""" & strName & "\"" " & strMax & "", "D:\RScripts\")
+
+        LblPlots.Text = "Running R - " & DateTime.Now() & "<br>"
+        LblPlots.Text &= "<img src='/RPlots/randhist.png'><br>"
+        LblPlots.Text &= "Returned:" & strReturned
+
+    End Sub
+
+
+    Function RunRScript(rpath, scriptpath, workdir)
+
+        Try
+            'rpath = C:\Program Files\R\R-3.4.3\bin\Rscript.exe
+
+            Dim startinfo = New ProcessStartInfo()
+            startinfo.FileName = rpath
+            startinfo.Arguments = scriptpath
+            startinfo.WorkingDirectory = workdir
+            startinfo.RedirectStandardOutput = True
+            startinfo.CreateNoWindow = True
+            startinfo.UseShellExecute = False
+
+            Using proc As Process = New Process()
+                proc.StartInfo = startinfo
+                proc.Start()
+                Return proc.StandardOutput.ReadToEnd()
+            End Using
+
+        Catch ex As Exception
+            Return "PROCESS ERROR:" & ex.ToString()
+        End Try
+
+        Return String.Empty
+    End Function
+End Class
